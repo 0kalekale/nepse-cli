f8f02688434b6e8bb4918f5589fa918d181271d4
0kalekale <kalekale.anon@gmail.com>
2021-02-16 16:32:35 +0545

    Clean up script and used /tmp/ for the files

--- scripts/parse.py
+++ scripts/parse.py
@@ -2,6 +2,6 @@ from html.parser import HTMLParser
 class MyHTMLParser(HTMLParser):
     def handle_data(self, data):
         print(data)        
-html = open("stonk.html", "r").read()
+html = open("/tmp/stonk.html", "r").read()
 parse = MyHTMLParser()
 parse.feed(html)
